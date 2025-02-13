705774f794d75bf5348f99fd86c049cba6b45fd8
Syed Abdul Kareem Ahmed <160421748048@mjcollege.ac.in>
2025-02-13 19:20:19 +0530

    Update MD5.java

--- MD5.java
+++ MD5.java
@@ -1,24 +1,27 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-public class MD5 {
-	public static void main(String[] args) {
-    	String input = "Hello, world!";
-    	String md5Hash = md5(input);
-        System.out.println("MD5 hash: " + md5Hash);
-	}
-	private static String md5(String input) {
-    	try {
-            MessageDigest md5Digest = MessageDigest.getInstance("MD5");
-        	byte[] inputBytes = input.getBytes();
-        	byte[] digestBytes = md5Digest.digest(inputBytes);
-       	 StringBuilder sb = new StringBuilder();
-        	for (byte b : digestBytes) {
-                sb.append(String.format("%02x", b));
-        	}
-        	return sb.toString();
-    	} catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-    	}
-    	return null;
-	}
+
+public class MD5{
+    private static String md5(String input){
+        try{
+            MessageDigest md5Digest=MessageDigest.getInstance("MD5");
+            byte[] inputBytes=input.getBytes();
+            byte[] digestBytes=md5Digest.digest(inputBytes);
+
+            StringBuilder sb=new StringBuilder();
+            for(byte b : digestBytes){
+                sb.append(String.format("%02x",b));
+            }
+            return sb.toString();
+        }
+        catch(NoSuchAlgorithmException e){
+            e.printStackTrace();;
+        }
+        return null;
+    }    
+    public static void main(String[] args){
+        String input="Hello, World!";
+        String md5Hash=md5(input);
+        System.out.println("MD5 Hash:"+md5Hash);
+    }
 }
